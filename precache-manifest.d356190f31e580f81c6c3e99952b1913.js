27a87b15760c79859917fc5074b2fd2b7d8e7759
tomer2077 <tfaibish@gmail.com>
2019-12-07 09:54:18 +0200

    Updates

---
+++ precache-manifest.d356190f31e580f81c6c3e99952b1913.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7c4ff796b34dadbca4c02a3a035def",
+    "url": "/explore-star-wars-planets/index.html"
+  },
+  {
+    "revision": "8e68628e9024f5e561b5",
+    "url": "/explore-star-wars-planets/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "7d608ff5a60797eb0ed7",
+    "url": "/explore-star-wars-planets/static/css/main.78ac1b7c.chunk.css"
+  },
+  {
+    "revision": "8e68628e9024f5e561b5",
+    "url": "/explore-star-wars-planets/static/js/2.0e9ba5ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/explore-star-wars-planets/static/js/2.0e9ba5ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d608ff5a60797eb0ed7",
+    "url": "/explore-star-wars-planets/static/js/main.57ddf8c0.chunk.js"
+  },
+  {
+    "revision": "2f3b0bada0857bb94671",
+    "url": "/explore-star-wars-planets/static/js/runtime-main.9d2a6dd2.js"
+  },
+  {
+    "revision": "972105bfc292d989b020f7fc29c62e60",
+    "url": "/explore-star-wars-planets/static/media/StarJedi.972105bf.woff"
+  },
+  {
+    "revision": "a4eaedfea50652fbebb63310de1ba82c",
+    "url": "/explore-star-wars-planets/static/media/StarJedi.a4eaedfe.woff2"
+  },
+  {
+    "revision": "8724746872c557a75e2a87d5385630ca",
+    "url": "/explore-star-wars-planets/static/media/climate.87247468.png"
+  },
+  {
+    "revision": "914c8d70f3f3d250d1892193d2ae478e",
+    "url": "/explore-star-wars-planets/static/media/earth.914c8d70.png"
+  },
+  {
+    "revision": "426788b4c364d635c6e95e22a97c2d23",
+    "url": "/explore-star-wars-planets/static/media/graphic-design.426788b4.png"
+  },
+  {
+    "revision": "53a8df5605ff1451d9d0c6711faa9027",
+    "url": "/explore-star-wars-planets/static/media/gravity.53a8df56.png"
+  },
+  {
+    "revision": "f078383442be6d50f845d84ca80b8cdd",
+    "url": "/explore-star-wars-planets/static/media/population.f0783834.png"
+  }
+]);
